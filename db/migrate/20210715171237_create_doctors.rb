5e369049777b08980163a60ff34d56d73f554220
mattjmason <masonmj10@gmail.com>
2021-07-15 13:33:56 -0400

    Done.

---
+++ db/migrate/20210715171237_create_doctors.rb
@@ -0,0 +1,12 @@
+class CreateDoctors < ActiveRecord::Migration[5.0]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
