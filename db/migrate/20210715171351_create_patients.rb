139b20ba6959d1c6f97d28c7c804761c76b6781e
mattjmason <masonmj10@gmail.com>
2021-07-15 13:33:51 -0400

    commit tree 8df60937f6403ddfeec5864547bf53ebd1b24766

---
+++ db/migrate/20210715171351_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
